67965aac95c5c4595d2daec1b6bf99534dad817c
agus suparlan <user@LAPTOP-EOSEVHU6>
2024-11-21 21:51:40 +0700

    first commit

---
+++ src/scripts/global/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  KEY: '93e5763d0be3a2bf97d52d00423463cf',
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
+  DATABASE_NAME: 'sigmarestoDB',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurant',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
